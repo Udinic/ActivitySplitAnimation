ba77ce45f6927f48bb7d19318ecf3dbad7401b45
Anton Nurdin Tuhadiansyah <anton>
2014-04-08 10:12:18 +0700

    Create main menu

---
+++ src/com/udinic/ActivitySplitAnimation/MainActivity.java
@@ -0,0 +1,50 @@
+package com.udinic.ActivitySplitAnimation;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+	
+    private class Sample {
+        private CharSequence title;
+        private Class<? extends Activity> activityClass;
+
+        public Sample(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
+        }
+
+        @Override
+        public String toString() {
+            return title.toString();
+        }
+    }
+
+    private static Sample[] mSamples;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instantiate the list of samples.
+        mSamples = new Sample[]{
+                new Sample(R.string.title_activity, Activity1.class),
+                new Sample(R.string.title_fragment, FragmentActivity.class),
+        };
+
+        setListAdapter(new ArrayAdapter<Sample>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                mSamples));
+    }
+
+    @Override
+    protected void onListItemClick(ListView listView, View view, int position, long id) {
+        startActivity(new Intent(MainActivity.this, mSamples[position].activityClass));
+    }
+}
